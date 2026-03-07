51074f372f56b29df93885eb07daef8ef6d17d80
subhajitpramanik007 <subho.p.1011@gmail.com>
2026-03-07 10:53:46 +0530

    feat(console-app): Update assets, implement dynamic logo routing based on authentication, and add a text label to the logo component.

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,11 +1,18 @@
+import { useAuth } from '@/contexts/AuthContext'
 import { cn } from '@/lib/utils'
 import { Link } from '@tanstack/react-router'
 import * as React from 'react'
 
 export const Logo: React.FC<{ className?: string }> = ({ className }) => {
+    // is authenticated
+    const { isAuthenticated } = useAuth()
+
+    const to = isAuthenticated ? '/dashboard' : '/'
+
     return (
-        <Link to="/">
+        <Link to={to} className="inline-flex items-center select-none">
             <img src="/wl_logo.png" alt="Logo" className={cn('h-12 w-auto', className)} />
+            <span className="text-xl font-bold tracking-tighter text-emerald-500">WorkLayer</span>
         </Link>
     )
 }
